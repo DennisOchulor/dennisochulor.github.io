c34adfe306946d130a1ee44d769017e617e5c34e
Dennis Ochulor <dennisnwanevu@gmail.com>
2024-08-31 16:05:06 +0800

    remove cors-proxy

--- cors-proxy/index.js
+++
@@ -1,7 +0,0 @@
-url = new URL(window.location.href)
-fetchUrl = url.searchParams.get("goto")
-fetch(fetchUrl)
-.then((response) => response.text())
-.then((text) => {
-    document.getElementById("theOne").innerText = text
-})
